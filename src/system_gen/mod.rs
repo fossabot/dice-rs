8950a6a043ef10b3234c161dc9d3a98b9df129e3
Victor M. Suarez <svmnotn@gmail.com>
2017-10-20 14:32:13 -0400

    start system gen

--- src/system_gen/mod.rs
+++ src/system_gen/mod.rs
@@ -1,3 +1,24 @@
 //!
 //! Generate a star system
 //!
+pub struct System {
+    name: String,
+    stars: Vec<Star>,
+    objects: Vec<Object>,
+}
+
+pub struct Star {
+    name: String,
+    class: String,
+    desc: String,
+}
+
+pub struct Object {
+    name: String,
+    ty: Type,
+    in_orbit: Vec<Object>,
+}
+
+pub enum Type {
+    Planet, Belt, Gas, Cloud
+}
