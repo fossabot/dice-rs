53e63911b0791f1f95e87f6ea84646ea804b7151
Victor M. Suarez <svmnotn@gmail.com>
2017-11-03 14:06:18 -0400

    add star gen

--- src/system_gen/mod.rs
+++ src/system_gen/mod.rs
@@ -27,19 +27,79 @@ pub enum Type {
 }
 
 use super::{dice, gen, Config};
+use super::config::StarType;
 
 pub fn generate(mut c: Config) -> System {
-  let num_planets = if (c.chance_avg * 100.0) > dice(100) as f32 {
-    c.avg_planets
-  } else {
-    gen(c.min_planets, c.max_planets)
-  };
-
-  let planets: Vec<Object> = Vec::with_capacity(num_planets as usize);
-
   c.star_types.sort_by_key(|t| (t.chance * 100_000.0) as u32);
 
-  println!("Star Types: {:?}", c.star_types);
+  let star = dice(100_000);
+  let mut prev_star: Option<&StarType> = None;
+  let mut found_star: Option<&StarType> = None;
+  for curr in &c.star_types {
+    if let Some(prev) = prev_star {
+      if star < (prev.chance * 100_000.0) as u32 && star > (curr.chance * 100_000.0) as u32 {
+        found_star = Some(curr);
+        break;
+      }
+    } else if star >= (curr.chance * 100_000.0) as u32 {
+      found_star = Some(curr);
+      break;
+    }
+
+    prev_star = Some(curr);
+  }
+
+  if let None = found_star {
+    found_star = c.star_types.last();
+  }
+
+  if let Some(star) = found_star {
+    let mut star_count = 1;
+    let multy = dice(100);
+    let mut companion: Option<&StarType> = None;
+    if (star.make_multy * 100.0) as u32 > multy {
+      for curr in &c.star_types {
+        if let Some(prev) = prev_star {
+          if multy < (prev.part_multy * 100.0) as u32 && multy > (curr.part_multy * 100.0) as u32 {
+            companion = Some(curr);
+            star_count += 1;
+            break;
+          }
+        } else if multy >= (curr.part_multy * 100.0) as u32 {
+          companion = Some(curr);
+          star_count += 1;
+          break;
+        }
+
+        prev_star = Some(curr);
+      }
+    }
+
+    let mut stars = Vec::with_capacity(star_count);
+    stars.push(Star {
+      name: "Main".into(),
+      class: star.class.clone(),
+      desc: star.description.clone(),
+    });
+
+    if let Some(comp) = companion {
+      stars.push(Star {
+        name: "Other".into(),
+        class: comp.class.clone(),
+        desc: comp.description.clone(),
+      });
+    }
+    
+    let num_planets = if (c.chance_avg * 100.0) > dice(100) as f32 {
+      c.avg_planets
+    } else {
+      gen(c.min_planets, c.max_planets)
+    };
+
+    let num_objects = num_planets + 2;
+
+    let objects: Vec<Object> = Vec::with_capacity(num_objects as usize);
+  }
 
   unimplemented!()
 }
